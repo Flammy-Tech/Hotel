b67fc6bec87c1470720ffa131c639e9df86f5a75
Lewis Ng'ang'a <59686000+lewis-2000@users.noreply.github.com>
2023-10-04 14:22:54 +0300

    Routes
    Added express routes

---
+++ index.js
@@ -0,0 +1,33 @@
+const express =require('express')
+const app =express()
+const port =3000
+
+app.use(express.static('public'))
+app.use('css', express.static(__dirname + 'public/css'))
+app.use('js', express.static(__dirname + 'public/js'))
+app.use('img', express.static(__dirname + 'public/img'))
+
+app.get('/', (req,res)=>{
+    res.sendFile(__dirname + '/public/pages/home.html')
+})
+
+app.get('/orders', (req,res)=>{
+    res.sendFile(__dirname + '/public/pages/orders.html')
+})
+
+app.get('/signin', (req,res)=>{
+    res.sendFile(__dirname + '/public/pages/signin.html')
+})
+
+app.post('/signin', (req,res)=>{
+    res.sendFile(__dirname + '/public/pages/signin.html')
+})
+
+app.get('/download', (req,res)=>{
+    res.download('index.js')
+})
+
+
+app.listen(port, ()=>{
+    console.log(`App listening on port ${port}`)
+})
---
+++ public/js/home.js
@@ -0,0 +1,5 @@
+const learnMore = document.getElementsByClassName('btn');
+
+learnMore.addEventListener('click', function (){
+    console.log('clicked');
+});
